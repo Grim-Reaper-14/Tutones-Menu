980c93accf3591e96fef4b12d48dfc8b0e77c6cb
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-17 16:45:55 -0400

    Implement menu input routing

---
+++ src/ui/Input.cpp
@@ -0,0 +1,202 @@
+#include "Input.hpp"
+
+#include "../core/logging/Logger.hpp"
+#include "../hooking/Win32Hook.hpp"
+
+namespace Tutones::UI
+{
+    Input& Input::Get() noexcept
+    {
+        static Input instance;
+        return instance;
+    }
+
+    bool Input::Initialize() noexcept
+    {
+        bool expected = false;
+        if (!m_Initialized.compare_exchange_strong(expected, true, std::memory_order_acq_rel))
+        {
+            TUTONES_LOG_TRACE("input", "Input initialize requested while already initialized");
+            return true;
+        }
+
+        m_MenuOpen.store(false, std::memory_order_release);
+        m_PendingActions.store(0, std::memory_order_release);
+        Hooking::Win32Hook::Get().SetMessageHandler(&Input::HandleWindowMessage);
+
+        TUTONES_LOG_INFO("input", "Win32 menu input routing initialized");
+        TUTONES_LOG_INFO("input", "Controls: F4 toggle, NUM8 up, NUM2 down, NUM4 left, NUM6 right, NUM5 select, NUM0 back");
+        TUTONES_LOG_DEBUG("input", "Backspace is intentionally not bound to menu Back");
+        return true;
+    }
+
+    void Input::Shutdown() noexcept
+    {
+        if (!m_Initialized.exchange(false, std::memory_order_acq_rel))
+        {
+            TUTONES_LOG_TRACE("input", "Input shutdown requested while not initialized");
+            return;
+        }
+
+        Hooking::Win32Hook::Get().SetMessageHandler(nullptr);
+        m_MenuOpen.store(false, std::memory_order_release);
+        m_PendingActions.store(0, std::memory_order_release);
+        TUTONES_LOG_INFO("input", "Win32 menu input routing stopped");
+    }
+
+    bool Input::IsInitialized() const noexcept
+    {
+        return m_Initialized.load(std::memory_order_acquire);
+    }
+
+    bool Input::IsMenuOpen() const noexcept
+    {
+        return m_MenuOpen.load(std::memory_order_acquire);
+    }
+
+    void Input::SetMenuOpen(bool open) noexcept
+    {
+        const auto previous = m_MenuOpen.exchange(open, std::memory_order_acq_rel);
+        if (previous == open)
+            return;
+
+        TUTONES_LOG_INFO("input", open ? "Tutones menu opened" : "Tutones menu closed");
+    }
+
+    void Input::ToggleMenu() noexcept
+    {
+        SetMenuOpen(!IsMenuOpen());
+    }
+
+    std::uint32_t Input::ConsumePendingActions() noexcept
+    {
+        return m_PendingActions.exchange(0, std::memory_order_acq_rel);
+    }
+
+    bool Input::Consume(InputAction action) noexcept
+    {
+        const auto mask = ToMask(action);
+        auto current = m_PendingActions.load(std::memory_order_acquire);
+
+        while ((current & mask) != 0)
+        {
+            const auto desired = current & ~mask;
+            if (m_PendingActions.compare_exchange_weak(
+                    current,
+                    desired,
+                    std::memory_order_acq_rel,
+                    std::memory_order_acquire))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool Input::HandleWindowMessage(HWND, UINT message, WPARAM wParam, LPARAM lParam) noexcept
+    {
+        auto& input = Get();
+        if (!input.IsInitialized())
+            return false;
+
+        if (message != WM_KEYDOWN && message != WM_SYSKEYDOWN)
+            return false;
+
+        if (IsRepeat(lParam))
+            return IsMenuKey(wParam) && input.IsMenuOpen();
+
+        switch (wParam)
+        {
+        case VK_F4:
+            input.ToggleMenu();
+            return true;
+
+        case VK_NUMPAD8:
+            if (input.IsMenuOpen())
+            {
+                input.Queue(InputAction::Up, "Up");
+                return true;
+            }
+            break;
+
+        case VK_NUMPAD2:
+            if (input.IsMenuOpen())
+            {
+                input.Queue(InputAction::Down, "Down");
+                return true;
+            }
+            break;
+
+        case VK_NUMPAD4:
+            if (input.IsMenuOpen())
+            {
+                input.Queue(InputAction::Left, "Left");
+                return true;
+            }
+            break;
+
+        case VK_NUMPAD6:
+            if (input.IsMenuOpen())
+            {
+                input.Queue(InputAction::Right, "Right");
+                return true;
+            }
+            break;
+
+        case VK_NUMPAD5:
+            if (input.IsMenuOpen())
+            {
+                input.Queue(InputAction::Select, "Select");
+                return true;
+            }
+            break;
+
+        case VK_NUMPAD0:
+            if (input.IsMenuOpen())
+            {
+                input.Queue(InputAction::Back, "Back");
+                return true;
+            }
+            break;
+
+        case VK_BACK:
+            if (input.IsMenuOpen())
+                TUTONES_LOG_TRACE("input", "Backspace ignored; menu Back is NUM0 only");
+            break;
+
+        default:
+            break;
+        }
+
+        return false;
+    }
+
+    bool Input::IsRepeat(LPARAM lParam) noexcept
+    {
+        return (lParam & (1ll << 30)) != 0;
+    }
+
+    bool Input::IsMenuKey(WPARAM key) noexcept
+    {
+        switch (key)
+        {
+        case VK_F4:
+        case VK_NUMPAD8:
+        case VK_NUMPAD2:
+        case VK_NUMPAD4:
+        case VK_NUMPAD6:
+        case VK_NUMPAD5:
+        case VK_NUMPAD0:
+            return true;
+        default:
+            return false;
+        }
+    }
+
+    void Input::Queue(InputAction action, const char* actionName) noexcept
+    {
+        m_PendingActions.fetch_or(ToMask(action), std::memory_order_acq_rel);
+        TUTONES_LOG_DEBUG("input", actionName);
+    }
+}
