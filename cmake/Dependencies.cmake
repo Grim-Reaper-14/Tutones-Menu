cab27829fdf5bd455e8c54f286c5ea6686b3ab0d
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-17 16:01:04 -0400

    Add pinned build dependencies

---
+++ cmake/Dependencies.cmake
@@ -0,0 +1,40 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    minhook
+    GIT_REPOSITORY https://github.com/TsudaKageyu/minhook.git
+    GIT_TAG v1.3.4
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG v1.92.9b
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_MakeAvailable(minhook imgui)
+
+add_library(tutones_imgui STATIC
+    ${imgui_SOURCE_DIR}/imgui.cpp
+    ${imgui_SOURCE_DIR}/imgui_draw.cpp
+    ${imgui_SOURCE_DIR}/imgui_tables.cpp
+    ${imgui_SOURCE_DIR}/imgui_widgets.cpp
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_dx12.cpp
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_win32.cpp
+)
+
+target_include_directories(tutones_imgui PUBLIC
+    ${imgui_SOURCE_DIR}
+    ${imgui_SOURCE_DIR}/backends
+)
+
+target_link_libraries(tutones_imgui PUBLIC
+    d3d12
+    dxgi
+)
+
+if(TARGET minhook)
+    target_include_directories(minhook PUBLIC ${minhook_SOURCE_DIR}/include)
+endif()
