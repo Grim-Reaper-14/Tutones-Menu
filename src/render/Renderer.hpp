9a7304395cfd9c0439bc34403bc1d335504ccfb0
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-17 16:50:36 -0400

    Add D3D12 frame resources

--- src/render/Renderer.hpp
+++ src/render/Renderer.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "RendererStatus.hpp"
+#include "d3d12/FrameContext.hpp"
+
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
 
 #include <cstdint>
-
-struct ID3D12Device;
-struct ID3D12DescriptorHeap;
-struct ID3D12CommandQueue;
-struct IDXGISwapChain3;
+#include <mutex>
+#include <vector>
 
 namespace Tutones::Render
 {
@@ -20,7 +22,8 @@ namespace Tutones::Render
         bool InitializeD3D12(IDXGISwapChain3* swapChain, ID3D12CommandQueue* commandQueue) noexcept;
         void BeginFrame() noexcept;
         void RenderFrame() noexcept;
-        void OnResize(std::uint32_t width, std::uint32_t height) noexcept;
+        void BeforeResize() noexcept;
+        void AfterResize(std::uint32_t width, std::uint32_t height) noexcept;
         void Shutdown() noexcept;
 
         [[nodiscard]] RendererStatus Status() const noexcept;
@@ -35,11 +38,44 @@ namespace Tutones::Render
         Renderer(const Renderer&) = delete;
         Renderer& operator=(const Renderer&) = delete;
 
+        bool CreateFrameResources() noexcept;
+        void ReleaseFrameResources() noexcept;
+        bool InitializeImGui(HWND window) noexcept;
+        void ShutdownImGui() noexcept;
+        bool WaitForFrame(D3D12::FrameContext& frame) noexcept;
+        void WaitForGpuIdle() noexcept;
+
+        static void AllocateSrvDescriptor(
+            struct ImGui_ImplDX12_InitInfo* info,
+            D3D12_CPU_DESCRIPTOR_HANDLE* cpu,
+            D3D12_GPU_DESCRIPTOR_HANDLE* gpu);
+        static void FreeSrvDescriptor(
+            struct ImGui_ImplDX12_InitInfo* info,
+            D3D12_CPU_DESCRIPTOR_HANDLE cpu,
+            D3D12_GPU_DESCRIPTOR_HANDLE gpu);
+
         RendererStatus m_Status{RendererStatus::NotInitialized};
         ID3D12Device* m_Device{};
         ID3D12CommandQueue* m_CommandQueue{};
         IDXGISwapChain3* m_SwapChain{};
+        ID3D12DescriptorHeap* m_RtvHeap{};
+        ID3D12DescriptorHeap* m_SrvHeap{};
+        ID3D12GraphicsCommandList* m_CommandList{};
+        ID3D12Fence* m_Fence{};
+        HANDLE m_FenceEvent{};
+        HWND m_Window{};
+
+        std::vector<D3D12::FrameContext> m_Frames;
+        std::vector<std::uint32_t> m_FreeSrvIndices;
+        std::mutex m_SrvMutex;
+
+        std::uint64_t m_NextFenceValue{1};
+        std::uint32_t m_RtvIncrement{};
+        std::uint32_t m_SrvIncrement{};
         std::uint32_t m_Width{};
         std::uint32_t m_Height{};
+        DXGI_FORMAT m_RtvFormat{DXGI_FORMAT_R8G8B8A8_UNORM};
+        bool m_ImGuiInitialized{};
+        bool m_FrameBegun{};
     };
 }
