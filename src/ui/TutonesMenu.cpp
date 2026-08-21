358cd05a59be3bf104a2dfe5006d86f7c7bb90a7
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-21 14:31:36 -0400

    register misc menu category

--- src/ui/TutonesMenu.cpp
+++ src/ui/TutonesMenu.cpp
@@ -1,3 +1,30 @@
+#include "MiscPanel.hpp"
+
+#define Categories BaseCategories
 #include "TutonesMenu.part00.inc"
+#undef Categories
+
+namespace Tutones::UI
+{
+    namespace
+    {
+        constexpr std::array<CategoryEntry, 10> Categories{{
+            BaseCategories[0],
+            BaseCategories[1],
+            BaseCategories[2],
+            BaseCategories[3],
+            BaseCategories[4],
+            BaseCategories[5],
+            BaseCategories[6],
+            BaseCategories[7],
+            BaseCategories[8],
+            {"M", "MISC",
+                {{"General", "HUD", "World", "Camera & Utilities"}},
+                {{"M", "H", "O", "C"}},
+                {{"Gameplay convenience actions and shared quality-of-life controls.", "Always-on coordinates, heading, FPS and Online session overlays.", "Local clock, freeze-time, weather and blackout controls.", "Gameplay camera telemetry/shake suppression plus player cleanup, parachute and underwater utilities."}}, 4, true},
+        }};
+    }
+}
+
 #include "TutonesMenu.part01.inc"
-#include "TutonesMenu.part02.inc"
+#include "TutonesMenu.part03.inc"
