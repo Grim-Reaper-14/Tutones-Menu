7b474e3199fd7c405f90312f4a96787ba3a87cc7
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-24 13:44:29 -0400

    Make V12 red dashboard the default theme

--- src/ui/ThemeDefinition.hpp
+++ src/ui/ThemeDefinition.hpp
@@ -8,7 +8,7 @@ namespace Tutones::UI
 {
     struct ThemeDefinition final
     {
-        std::string name{"Default V11"};
+        std::string name{"Tutones V12 Red"};
         ImVec4 accent{};
         ImVec4 accentHover{};
         ImVec4 accentDark{};
@@ -25,7 +25,7 @@ namespace Tutones::UI
         std::string headerImage;
         std::string footerImage;
         std::string backgroundImage;
-        float backgroundOpacity{0.36f};
+        float backgroundOpacity{0.18f};
         std::string fontFile;
         float fontSize{15.0f};
     };
@@ -33,19 +33,19 @@ namespace Tutones::UI
     inline ThemeDefinition DefaultTheme() noexcept
     {
         ThemeDefinition t;
-        t.accent = {2.f/255.f,108.f/255.f,249.f/255.f,1.f};
-        t.accentHover = {15.f/255.f,143.f/255.f,239.f/255.f,1.f};
-        t.accentDark = {1.f/255.f,26.f/255.f,75.f/255.f,1.f};
-        t.windowBg = {1.f/255.f,3.f/255.f,7.f/255.f,1.f};
-        t.bodyBg = {4.f/255.f,7.f/255.f,12.f/255.f,1.f};
-        t.footerBg = {3.f/255.f,7.f/255.f,13.f/255.f,1.f};
-        t.railBg = {6.f/255.f,10.f/255.f,17.f/255.f,.96f};
-        t.panelBg = {7.f/255.f,11.f/255.f,18.f/255.f,.98f};
-        t.panelBorder = {2.f/255.f,108.f/255.f,249.f/255.f,.28f};
-        t.separator = {2.f/255.f,108.f/255.f,249.f/255.f,.22f};
-        t.controlBg = {10.f/255.f,16.f/255.f,25.f/255.f,1.f};
-        t.controlHover = {9.f/255.f,34.f/255.f,66.f/255.f,1.f};
-        t.mutedText = {139.f/255.f,153.f/255.f,177.f/255.f,1.f};
+        t.accent = {229.f/255.f,42.f/255.f,48.f/255.f,1.f};
+        t.accentHover = {248.f/255.f,66.f/255.f,72.f/255.f,1.f};
+        t.accentDark = {74.f/255.f,10.f/255.f,15.f/255.f,1.f};
+        t.windowBg = {3.f/255.f,5.f/255.f,8.f/255.f,1.f};
+        t.bodyBg = {5.f/255.f,8.f/255.f,12.f/255.f,1.f};
+        t.footerBg = {4.f/255.f,7.f/255.f,10.f/255.f,1.f};
+        t.railBg = {7.f/255.f,10.f/255.f,14.f/255.f,.985f};
+        t.panelBg = {9.f/255.f,12.f/255.f,17.f/255.f,.985f};
+        t.panelBorder = {78.f/255.f,83.f/255.f,91.f/255.f,.38f};
+        t.separator = {90.f/255.f,94.f/255.f,103.f/255.f,.26f};
+        t.controlBg = {12.f/255.f,16.f/255.f,22.f/255.f,1.f};
+        t.controlHover = {35.f/255.f,18.f/255.f,23.f/255.f,1.f};
+        t.mutedText = {151.f/255.f,157.f/255.f,168.f/255.f,1.f};
         return t;
     }
 }
