65cee5a2d12ac538ae7ad25ce06b30849c1fbc31
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-26 08:22:41 -0400

    Add Enhanced netshop basket natives

--- src/game/NetshoppingNatives.hpp
+++ src/game/NetshoppingNatives.hpp
@@ -13,6 +13,16 @@
 
 namespace Tutones::Game::NetshoppingNatives
 {
+    struct BasketItem final
+    {
+        std::int64_t primaryHash{};
+        std::int64_t secondaryHash{};
+        std::int64_t value{};
+        std::int64_t statValue{};
+    };
+
+    static_assert(sizeof(BasketItem) == 4 * sizeof(std::int64_t));
+
     namespace Detail
     {
         struct NativeProgram final
@@ -34,6 +44,10 @@ namespace Tutones::Game::NetshoppingNatives
             CatalogItemKeyIsValid,
             GetPrice,
             BeginService,
+            BasketStart,
+            BasketEnd,
+            BasketIsActive,
+            BasketAddItem,
             CheckoutStart,
             HandlerCount,
         };
@@ -66,6 +80,10 @@ namespace Tutones::Game::NetshoppingNatives
                 0x206AC354EB77B7FDull, // NET_GAMESERVER_CATALOG_ITEM_KEY_IS_VALID
                 0xD2ACF01ED6E6D7C6ull, // NET_GAMESERVER_GET_PRICE
                 0x54BC5E0B6A29AE8Aull, // NET_GAMESERVER_BEGIN_SERVICE
+                0x5156B6B1D1CD58FEull, // NET_GAMESERVER_BASKET_START
+                0xD8EB47E09DFC393Cull, // NET_GAMESERVER_BASKET_END
+                0x79EEE2067838CC59ull, // NET_GAMESERVER_BASKET_IS_ACTIVE
+                0x5E8BF7E974BEA7D7ull, // NET_GAMESERVER_BASKET_ADD_ITEM
                 0x5F7C6361179DFFC4ull, // NET_GAMESERVER_CHECKOUT_START
             };
 
@@ -143,6 +161,63 @@ namespace Tutones::Game::NetshoppingNatives
         }
 
         Detail::Handlers()[Detail::BeginService](&context);
+        context.FixVectors();
+        return context.GetReturnValue<std::int32_t>() != 0;
+    }
+
+    [[nodiscard]] inline std::optional<bool> BasketIsActive() noexcept
+    {
+        if (!Detail::ResolveHandlers())
+            return std::nullopt;
+
+        Native::CallContext context;
+        Detail::Handlers()[Detail::BasketIsActive](&context);
+        return context.GetReturnValue<std::int32_t>() != 0;
+    }
+
+    [[nodiscard]] inline std::optional<bool> BasketEnd() noexcept
+    {
+        if (!Detail::ResolveHandlers())
+            return std::nullopt;
+
+        Native::CallContext context;
+        Detail::Handlers()[Detail::BasketEnd](&context);
+        return true;
+    }
+
+    [[nodiscard]] inline std::optional<bool> BasketStart(
+        int* transactionId,
+        Hash categoryHash,
+        Hash actionHash,
+        int flags = 4) noexcept
+    {
+        if (!transactionId || categoryHash == 0 || actionHash == 0 || !Detail::ResolveHandlers())
+            return std::nullopt;
+
+        Native::CallContext context;
+        if (!context.PushArg(transactionId)
+            || !context.PushArg(categoryHash)
+            || !context.PushArg(actionHash)
+            || !context.PushArg(flags))
+        {
+            return std::nullopt;
+        }
+
+        Detail::Handlers()[Detail::BasketStart](&context);
+        context.FixVectors();
+        return context.GetReturnValue<std::int32_t>() != 0;
+    }
+
+    [[nodiscard]] inline std::optional<bool> BasketAddItem(BasketItem* item, int quantity) noexcept
+    {
+        if (!item || quantity <= 0 || !Detail::ResolveHandlers())
+            return std::nullopt;
+
+        Native::CallContext context;
+        if (!context.PushArg(item) || !context.PushArg(quantity))
+            return std::nullopt;
+
+        Detail::Handlers()[Detail::BasketAddItem](&context);
         context.FixVectors();
         return context.GetReturnValue<std::int32_t>() != 0;
     }
